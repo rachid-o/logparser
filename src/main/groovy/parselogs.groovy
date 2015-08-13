43167db59cdb0766e42e03e1cf2629a56ebdd488
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2015-08-13 23:00:02 +0200

    Added example.log

--- src/main/groovy/parselogs.groovy
+++ src/main/groovy/parselogs.groovy
@@ -2,13 +2,15 @@ import com.xeiam.xchart.BitmapEncoder
 import com.xeiam.xchart.Chart
 import com.xeiam.xchart.ChartBuilder
 import com.xeiam.xchart.StyleManager
+import com.xeiam.xchart.SwingWrapper
 
 /**
  * Script to retrieve durations from logfiles and outputs it to CSV
  */
 
 
-def filename = args.length > 0 ? args[0]: "test.log"
+
+def filename = args.length > 0 ? args[0]: "example.log"
 println "Parsing: $filename"
 
 File logFile = new File(filename)  // inFile is a string file name
@@ -16,24 +18,26 @@ if(!logFile.exists()) {
     println "${filename} does not exist"
     return;
 }
-def csvFilename = filename + ".csv"
 
-def cvsFile = new File(csvFilename)
-
-def map = [:]
+def methodDurations = [:]
 def KEY_FINISHED = "FINISHED"
+def NEWLINE = System.getProperty("line.separator")
+def csvFilename = filename + ".csv"
+def cvsFile = new File(csvFilename)
+cvsFile.delete()
+
 logFile.eachLine{line ->
     if(line.contains("FINISHED")) {
         finishedLine = line.substring(line.lastIndexOf(KEY_FINISHED) + KEY_FINISHED.length()).trim()
         tokens = finishedLine.split('\\s')
         methodName = tokens[0]
         duration = tokens[1]
-        cvsFile.write("\"$methodName\";$duration")
+        cvsFile.append("\"$methodName\";$duration" + NEWLINE)
 
-        if(!map[methodName]) {
-            map[methodName] = [];
+        if(!methodDurations[methodName]) {
+            methodDurations[methodName] = [];
         }
-        map[methodName].add(duration.toInteger())
+        methodDurations[methodName].add(duration.toInteger())
     }
 }
 println "Created CSV: ${csvFilename}"
@@ -43,12 +47,12 @@ Chart chart = new ChartBuilder().chartType(StyleManager.ChartType.Line)
         .width(800).height(600).title(filename)
         .xAxisTitle("cycle").yAxisTitle("milliseconds").build();
 
-map.each{ method, durations ->
+methodDurations.each{ method, durations ->
     yData = (1..durations.size())    // List of index numbers of each element in durations
     chart.addSeries(method, yData, durations);
 }
 
 
-//new SwingWrapper(chart).displayChart();
+//new SwingWrapper(chart).displayChart();   // Show the chart
 BitmapEncoder.saveBitmap(chart, filename, BitmapEncoder.BitmapFormat.PNG);
 println "Created graph: ${filename}.png"
